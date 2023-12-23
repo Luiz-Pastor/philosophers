943e279caca7db22b6c349d04d198ef8ca842fea
Luiz-Pastor <luigpacr5@gmail.com>
2023-12-23 23:54:00 +0100

    Modified some variables in the main func

--- philo/src/manage.c
+++ philo/src/manage.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   manage.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
+/*   By: lpastor- <lpastor-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 10:27:15 by lpastor-          #+#    #+#             */
-/*   Updated: 2023/12/22 11:12:59 by lpastor-         ###   ########.fr       */
+/*   Updated: 2023/12/23 13:52:19 by lpastor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,15 @@
 
 void	*manage(void *data)
 {
-	t_philo	philo;
+	t_philo	*philo;
 
-	philo = *(t_philo *) data;
+	philo = (t_philo *) data;
 	
-	printf("Te saluda el filosofo numero %d\n", philo.id);
+	(void)philo;
+	/*pthread_mutex_lock(&philo->data->mutex_terminal);
+	printf("Te saluda el filosofo numero %d\n", philo->id);
+	usleep(1000000);
+	pthread_mutex_unlock(&philo->data->mutex_terminal);*/
+	
 	return (NULL);	
 }
