6f0015c78c41bff35ac5a10bd54bcdce2d84eb68
Luiz-Pastor <luigpacr5@gmail.com>
2023-12-24 13:52:29 +0100

    Added the struct of the manage function

--- philo/src/manage.c
+++ philo/src/manage.c
@@ -6,7 +6,7 @@
 /*   By: lpastor- <lpastor-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 10:27:15 by lpastor-          #+#    #+#             */
-/*   Updated: 2023/12/24 12:01:04 by lpastor-         ###   ########.fr       */
+/*   Updated: 2023/12/24 13:31:35 by lpastor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,29 @@ void	*manage(void *data)
 	philo = (t_philo *) data;
 	
 	/* Esperamos que todos los hilos se hayan creado para empezar */
-	printf("Esperando %d...\n", philo->id);
+	// printf("Esperando %d...\n", philo->id);
 	wait_start(philo->data);
-	
-	printf("Hola desde %d => [%ld]\n", philo->id, philo->data->start);
+	// printf("Hola desde %d => [%ld]\n", philo->id, philo->data->start);
 
+	/* Empieza la simulacion */
+	while (!is_finished(philo->data))
+	{
+		/*
+			ACCIONES:
+				1- Mirar si ya ha comido todo lo que deberia comer [parametro opcional]
+				2- Comer
+				3- Dormir
+				4- Pensar
+		*/
+
+		/* TODO: comer */
+
+		/* TODO: dormir */
+		print_status(philo, SLEEPING);
+		philo_sleep(philo);
+
+		/* TODO: pensar */
+		
+	}
 	return (NULL);	
 }
