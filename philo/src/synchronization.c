37f7221e305c45b8568a69a31887a4759a07e908
Luiz-Pastor <luigpacr5@gmail.com>
2023-12-24 13:54:24 +0100

    Optimize wait functions for threads

--- philo/src/synchronization.c
+++ philo/src/synchronization.c
@@ -6,7 +6,7 @@
 /*   By: lpastor- <lpastor-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 11:34:23 by lpastor-          #+#    #+#             */
-/*   Updated: 2023/12/24 11:51:17 by lpastor-         ###   ########.fr       */
+/*   Updated: 2023/12/24 12:25:38 by lpastor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,15 @@
 
 void	wait_start(t_data *data)
 {
+	int	start;
 	/* Bucle infinito hasta que `start` sea 1*/
 	while (1)
 	{
-		pthread_mutex_lock(&data->mutex_start);
-		if (data->start)
-		{
-			pthread_mutex_unlock(&data->mutex_start);
+		pthread_mutex_lock(&data->mutex_control);
+		start = data->start;
+		pthread_mutex_unlock(&data->mutex_control);
+		if (start)
 			break ;
-		}
-		pthread_mutex_unlock(&data->mutex_start);
 	}
 }
 
@@ -33,7 +32,16 @@ void	start(t_data *data)
 	data->start_time = get_instant();
 
 	/* Cambiamos la variable de start a 1*/
-	pthread_mutex_lock(&data->mutex_start);
+	pthread_mutex_lock(&data->mutex_control);
 	data->start = 1;
-	pthread_mutex_unlock(&data->mutex_start);
+	pthread_mutex_unlock(&data->mutex_control);
+}
+
+int	is_finished(t_data *data)
+{
+	int	end;
+	pthread_mutex_lock(&data->mutex_control);
+	end = data->end;
+	pthread_mutex_unlock(&data->mutex_control);
+	return (end);
 }
