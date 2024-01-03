a889913b8a8d16c9de830c8c7b9ce51bc7848913
Luiz-Pastor <luigpacr5@gmail.com>
2024-01-03 11:56:04 +0100

    Added the sleep action

--- philo/src/action_sleep.c
+++ philo/src/action_sleep.c
@@ -6,7 +6,7 @@
 /*   By: lpastor- <lpastor-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 13:18:53 by lpastor-          #+#    #+#             */
-/*   Updated: 2023/12/24 13:33:13 by lpastor-         ###   ########.fr       */
+/*   Updated: 2023/12/24 18:38:31 by lpastor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,16 @@ void	philo_sleep(t_philo *philo)
 	start_time = get_instant();
 	while (get_instant() - start_time < philo->data->time_to_sleep)
 	{
+		/* Si alguien ha muerto, dejamos de dormir */
 		if (is_finished(philo->data))
 			break ;
 		
+		/* Hacemos calculos de tiempos  */
 		diff = get_instant() - start_time;
 		remaining = philo->data->time_to_sleep - diff;
 
 		if (remaining > 1000)
-			usleep(philo->data->time_to_sleep / 2);
+			usleep(remaining / 2);
 		else
 		{
 			while (get_instant() - start_time < philo->data->time_to_sleep)
