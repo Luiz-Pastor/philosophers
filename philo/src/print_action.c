588b59ef6ed9a628ac625d99d21d01748cfe6665
Luiz-Pastor <luigpacr5@gmail.com>
2024-03-21 23:56:53 +0100

    Replaced the print func

---
+++ philo/src/print_action.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_action.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpastor- <lpastor-@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/21 23:23:28 by lpastor-          #+#    #+#             */
+/*   Updated: 2024/03/21 23:28:52 by lpastor-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+void	print_action(t_philo *philo, t_action action)
+{
+	size_t	actual_time;
+
+	actual_time = get_instant() - philo->data->start_time;
+
+	pthread_mutex_lock(&philo->data->mutex_terminal);
+	if (action == EATING && !is_finished(philo->data))
+		printf("%ld %d is eating\n", actual_time, philo->id);
+	else if (action == SLEEPING && !is_finished(philo->data))
+		printf("%ld %d is sleeping\n", actual_time, philo->id);
+	else if (action == THINKING && !is_finished(philo->data))
+		printf("%ld %d is thinking\n", actual_time, philo->id);
+	else if (action == FORK && !is_finished(philo->data))
+		printf("%ld %d has taken a fork\n", actual_time, philo->id);
+	else /* DEAD*/
+		printf("%ld %d is dead\n", actual_time, philo->id);
+	pthread_mutex_unlock(&philo->data->mutex_terminal);
+}
