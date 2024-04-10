34fcd8fb30973acd6687031db0d0a5f0d66a5458
Luiz-Pastor <luigpacr5@gmail.com>
2024-04-10 11:17:26 +0200

    Finished the action, added some dead checkers on all the possible parts

--- philo/src/eat_action.c
+++ philo/src/eat_action.c
@@ -6,7 +6,7 @@
 /*   By: lpastor- <lpastor-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 23:22:02 by lpastor-          #+#    #+#             */
-/*   Updated: 2024/03/22 11:39:22 by lpastor-         ###   ########.fr       */
+/*   Updated: 2024/04/10 11:13:55 by lpastor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,24 +26,22 @@ int	eat_action(t_philo *philo)
 {
 	/* Coger priemr tenerdor*/	
 	pthread_mutex_lock(philo->first_fork);
-	print_action(philo, FORK);
-
 	if (is_finished(philo))
 		return (1);
+	print_action(philo, FORK);
 
 	/* Coger segundo tenedor */
 	pthread_mutex_lock(philo->second_fork);
-	print_action(philo, FORK);
-
 	if (is_finished(philo))
 		return (1);
+	print_action(philo, FORK);
 
 	/* Comer --> esperar*/
 	print_action(philo, EATING);
 	usleep_better(philo->data->time_to_eat);
 
 	/* Antes de acabar de comer, miramos si esta muerto */
-	if (check_self_dead(philo))
+	if (is_finished(philo))
 	{
 		/* Desbloqueamos tenedores*/
 		unlock_forks(philo->first_fork, philo->second_fork);
@@ -51,7 +49,7 @@ int	eat_action(t_philo *philo)
 	}
 
 	/* Actualizar tiempos y cantidad de comidas */
-	philo->last_meal = get_instant() - philo->data->start_time;
+	philo->last_meal = get_instant();
 
 	pthread_mutex_lock(&philo->internal_mutex);
 	philo->meals_done++;
