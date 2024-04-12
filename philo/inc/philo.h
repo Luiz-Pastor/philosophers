d15188119b98a7e2c4f38f3c4a9a57e6d2f14bbc
Luiz-Pastor <luigpacr5@gmail.com>
2024-04-12 10:24:16 +0200

    Header norminnette fixed

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,10 +6,9 @@
 /*   By: lpastor- <lpastor-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 19:32:13 by lpastor-          #+#    #+#             */
-/*   Updated: 2024/04/12 10:10:59 by lpastor-         ###   ########.fr       */
+/*   Updated: 2024/04/12 10:23:52 by lpastor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef PHILO_H
 # define PHILO_H
@@ -21,136 +20,86 @@
 # include <sys/time.h>	/* gettimeofday		*/
 # include <pthread.h>	/* threads			*/
 
-/* NOTE: tiempos de argumentos en milisegundos
-	- 1 milisegundo = 1s		* 1000
-	- 1 milisegundo = 1micros	/ 1000
+/* NOTE: Time remember
+	- 1 mili = 1s		* 1000
+	- 1 mili = 1micro	/ 1000
 */
 # define CONVERSION_FACTOR 1000
 
 typedef struct s_data	t_data;
 typedef struct s_philo	t_philo;
 
-typedef enum {
+typedef enum s_action
+{
 	EATING,
 	SLEEPING,
 	THINKING,
 	FORK,
 	DIED
-} t_action;
+}			t_action;
 
-typedef enum {
+typedef enum t_dead
+{
 	NO_DEAD = 0,
 	SELF_DEAD,
 	ANOTHER_DEAD
-} t_dead;
+}			t_dead;
 
-/* Estructura con la información general */
+/* Struct with the main info */
 struct s_data
 {
-	/* Array de filosofos */
-	t_philo		*philos;			/* Ids de los hilos */
-	int			threads_active;
-	
-	/* Argumentos */
-	int			number_philo;	/* Numero de filósofos */
-	size_t		time_to_die;	/* Tiempo que debe pasar sin comer para que muera un philo */
-	size_t		time_to_eat;	/* Tiempo en comer */
-	size_t		time_to_sleep;	/* Tiempo en dormir */
-	int			meals_needed;		/* Numero de comidas [opcional]*/
-
-	/* Flag para empezar la simulacion */
-	size_t		start;
-
-	/* Tiempo en el que empezo la somulacion */
-	size_t		start_time;
-
-	/* Dead flag, si alguien muere se actualiza */
-	int			end;
-
-	/* Monitor de la simulacion */
-	pthread_t	monitor;
-
-	/* Declaracion de los mutex's */
-	pthread_mutex_t	mutex_control;	/* Usado para saber cuando empezar y cuando terminar */
-	pthread_mutex_t	mutex_terminal;	/* Para escribir los mensajes del status */
-	pthread_mutex_t	*forks;			/* Array de los mutex de los tenedores */
+	t_philo			*philos;			/* Pilo array */
+	int				threads_active;		/* Number of threads (syncro) */
+	int				number_philo;		/* Number of philos */
+	size_t			time_to_die;		/* Time to die */
+	size_t			time_to_eat;		/* Time to eat */
+	size_t			time_to_sleep;		/* Time to sleep */
+	int				meals_needed;		/* Meals needed (opcional) */
+	size_t			start;				/* Flag to start the simulation*/
+	size_t			start_time;			/* Time when the simulation started */
+	int				end;				/* Flag to end the simulation */
+	pthread_t		monitor;			/* Monitor thread */
+	pthread_mutex_t	mutex_control;		/* Start/end of the simulation */
+	pthread_mutex_t	mutex_terminal;		/* Print of msg on the terminal */
+	pthread_mutex_t	*forks;				/* Forks mutex */
 };
 
-/* Estructura de filosofo */
+/* Philo struct */
 struct s_philo
 {
-	/* Id del filosofo (indice de la tabla de filosofos + 1)*/
-	int				id;
-
-	/* Hilo del filosofo*/
-	pthread_t		thread;
-
-	/* Dead */
-	int				dead;
-	
-	/* Información de tiempos */
-	t_data			*data;
-
-	/* Informacion de comidas */
-	int				meals_done;	/* Numero de comidas*/
-	size_t			last_meal;	/* Checkear haze cuanto comio el filosofo, para saber si ha muerto o no*/
-	int				satisfied;	/* Ha hecho todas las comidas que tenia que hacer */
-
-	/* Mutex de los tenedores */
-	/*
-	pthread_mutex_t	*left;
-	pthread_mutex_t	*right;
-	*/
-	pthread_mutex_t	internal_mutex;	/* Usado para hacer operaciones internas */
-	pthread_mutex_t	*first_fork;
-	pthread_mutex_t	*second_fork;
+	int				id;				/* Filo id */
+	pthread_t		thread;			/* Thread id */
+	int				dead;			/* Dead flag */
+	t_data			*data;			/* General struct data */
+	int				meals_done;		/* Number of meals done */
+	size_t			last_meal;		/* Time of the last meal */
+	int				satisfied;		/* Flag to be satisfied */
+	pthread_mutex_t	internal_mutex;	/* Internal mutex */
+	pthread_mutex_t	*first_fork;	/* First fork mutex */
+	pthread_mutex_t	*second_fork;	/* Second fork mutex */
 };
 
-/* Convertions */
 size_t	ft_atull(char *str, int *flag);
 int		ft_strict_atoi(char *str, int *flag);
-
-/* Arguments */
-int	read_arguments(t_data *data, int argc, char **argv);
-
-/* Start env */
-int	init_data(t_data *data, int argc, char **argv);
-int	start_threads(t_data *data, void *(*routine)(void*));
-
-/* Free memory */
+int		read_arguments(t_data *data, int argc, char **argv);
+int		init_data(t_data *data, int argc, char **argv);
+int		start_threads(t_data *data, void *(*routine)(void*));
 void	delete_mutex(t_data *data);
 void	delete_data(t_data *data);
-
-/* Manage tasks */
 void	*one_philo(void *data);
 void	*philo_routine(void *arg);
 void	*monitor_routine(void *arg);
-
-/* Time functions */
 size_t	diff_time(struct timeval start, struct timeval end);
 size_t	get_instant(void);
 void	usleep_better(size_t time);
-
-/* Sincronización */
 void	start(t_data *data);
 void	wait_start(t_data *data);
 void	wait_monitor(t_data *data);
-
-/* Check end simulation */
-t_dead is_finished(t_philo *philo);
+t_dead	is_finished(t_philo *philo);
 t_dead	check_self_dead(t_philo *philo);
-//int		is_finished(t_data *data);
-
-/* Output */
 void	print_action(t_philo *philo, t_action action);
-//void	print_status(t_philo *philo, int status);
-
-/* Actions */
-int	eat_action(t_philo *philo);
-int is_satisfied(t_philo *philo);
+int		eat_action(t_philo *philo);
+int		is_satisfied(t_philo *philo);
 void	think_action(t_philo *philo);
-//void	philo_eat(t_philo *philo);
-//void	philo_sleep(t_philo *philo);
-//void	philo_think(t_philo *philo, int beginning);
 
 #endif
